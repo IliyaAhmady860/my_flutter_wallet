9e58189044c68a6e8506d321d6e6b555f88a20da
IliyaAhmady860 <iliyaahmady860@gmail.com>
2026-03-06 12:28:40 +0200

    made a custom button which pops two more icon button to use

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:fab_circular_menu_plus/fab_circular_menu_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '/providers/nav_bar_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -7,43 +9,41 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isVisible = ref.watch(navBarVisibilityProvider);
     final scrollController = ref.watch(scrollControllerProvider);
+
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      appBar: AppBar(title: const Text('Home')),
+      // Removed the top AppBar
       body: ListView.builder(
         controller: scrollController,
         itemCount: 40,
         itemBuilder: (context, index) => ListTile(title: Text('Item $index')),
       ),
-      bottomNavigationBar: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        height: isVisible ? 90.0 : 0.0,
-        child: Wrap(
-          children: [
-            BottomAppBar(
-              color: Colors.blueGrey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Center(
-                    child: Column(
-                      children: [
-                        FloatingActionButton(
-                          onPressed: () {},
-                          backgroundColor: Colors.blue,
-                          shape: const CircleBorder(),
-                          child: Icon(Icons.add),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FabCircularMenuPlus(
+        alignment: Alignment.bottomRight,
+        ringDiameter: 300.0,
+        ringWidth: 70.0,
+        fabSize: 70.0,
+        fabOpenIcon: const Icon(Icons.add, color: Colors.white, size: 40.0),
+        fabCloseIcon: const Icon(Icons.close, color: Colors.white, size: 40.0),
+        ringColor: Colors.transparent,
+        fabOpenColor: Colors.blueGrey,
+        fabCloseColor: Colors.blueGrey,
+        children: <Widget>[
+          IconButton(
+            style: IconButton.styleFrom(backgroundColor: Colors.blueGrey),
+            icon: const Icon(Icons.trending_down, size: 40),
+            onPressed: () {},
+            color: Colors.redAccent,
+          ),
+          IconButton(
+            style: IconButton.styleFrom(backgroundColor: Colors.blueGrey),
+            icon: const Icon(Icons.trending_up, size: 40),
+            onPressed: () {},
+            color: Colors.greenAccent,
+          ),
+        ],
       ),
     );
   }
